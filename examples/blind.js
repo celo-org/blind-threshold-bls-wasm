e1e2f8a1ab5154c2f0b1c55cb0d61fbb1d907208
Kobi Gurkan <kobigurk@gmail.com>
2020-04-29 00:39:12 +0300

    updates to latest version

--- examples/blind.js
+++ examples/blind.js
@@ -19,7 +19,7 @@ const private_key = keypair.privateKey
 const public_key = keypair.publicKey
 
 // Sign the user's blinded message with the service's private key
-const blind_sig = threshold.sign(private_key, blind_msg)
+const blind_sig = threshold.signBlindedMessage(private_key, blind_msg)
 
 // User unblinds the signature with this scalar
 const unblinded_sig = threshold.unblind(blind_sig, blinded_msg.blindingFactor)
